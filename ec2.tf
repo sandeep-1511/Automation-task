0cb08b62a208f449de199fd0f2960716c5dfa4d6
sandeep-1511 <108406587+sandeep-1511@users.noreply.github.com>
2023-05-11 14:20:43 +0530

    Add files via upload

---
+++ ec2.tf
@@ -0,0 +1,15 @@
+#instance creation
+resource "aws_instance" "lmsfront" {
+  ami                         =  "ami-0d09654d0a20d3ae2"
+  instance_type               = "t2.micro"
+  subnet_id                   = aws_subnet.subnet1.id
+  vpc_security_group_ids      = [aws_security_group.ssh1_group.id]
+  availability_zone           = "ap-south-1"
+  associate_public_ip_address = true
+  key_name                    = "TF-key"
+  tags = {
+    Name = "frontend-webapp"
+  }
+}
+
+  
