47b276f5c34bc3267a5ba3ea0268a471ecc489fd
sandeep-1511 <108406587+sandeep-1511@users.noreply.github.com>
2023-05-11 13:27:31 +0530

    Add files via upload

---
+++ nat.tf
@@ -0,0 +1,26 @@
+#nate gateway creation
+resource "aws_eip" "nat-1" {}
+
+resource "aws_nat_gateway" "nat1" {
+  allocation_id = aws_eip.nat-1.id
+  subnet_id     = aws_subnet.subnet1.id
+  tags = {
+    Name = nat-1
+ }  
+}
+
+resource "aws_eip" "nat-2" {}
+
+resource "aws_nat_gateway" "nat2" {
+  allocation_id = aws_eip.nat-2.id
+  subnet_id     = aws_subnet.subnet2.id
+  tags = {
+    Name = nat-2
+ }  
+}
+
+
+
+
+
+
