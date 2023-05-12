41bb30f3bf8f3efcc91992e626dec0c4151af088
sandeep-1511 <108406587+sandeep-1511@users.noreply.github.com>
2023-05-12 11:40:33 +0530

    Update security.tf

--- security.tf
+++ security.tf
@@ -2,8 +2,7 @@
   resource "aws_security_group" "ssh1_group" {
   name        = "ssh1"
   description = "ssh1 security group"
-  vpc_id = aws_vpc.main_digital.id
-
+  vpc_id = aws_vpc.appvpc.id
 
 ingress {
     from_port   = 22
@@ -24,17 +23,3 @@ output "security_group_id" {
 }
 
 
-resource "aws_key_pair" "TF_key" {
-  key_name   = "TF-key"
-  public_key = tls_private_key.rsa.public_key_openssh 
-}
-
-resource "tls_private_key" "rsa" {
-  algorithm = "RSA"
-  rsa_bits  = 4096
-}
-
-resource "local_file" "TF-key" {
-  content  = tls_private_key.rsa.private_key_pem
-  filename = tfkey
-}
