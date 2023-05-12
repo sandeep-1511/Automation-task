528229915c1066f62da5c19c15be944442b338eb
sandeep-1511 <108406587+sandeep-1511@users.noreply.github.com>
2023-05-12 14:16:55 +0530

    Update security-2.tf

--- security-2.tf
+++ security-2.tf
@@ -1,5 +1,5 @@
 #create security group
-  resource "aws_security_group" "ssh2" {
+  resource "aws_security_group" "ssh2_group" {
   name        = "ssh2"
   description = "ssh2 security group"
   vpc_id = aws_vpc.dbvpc.id
@@ -12,14 +12,20 @@ ingress {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+ingress {
+  from_port   = -1
+  to_port     = -1
+  protocol    = "icmp"
+  cidr_blocks = ["aws_security_group.ssh1"]
+}
   egress {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
 }
 output "security_group_id-2" {
-  value = aws_security_group.ssh2.id
+  value = aws_security_group.ssh2_group.id
 }
-
