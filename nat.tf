d676ad46806c8d05d384ae9430382f99a7211850
sandeep-1511 <108406587+sandeep-1511@users.noreply.github.com>
2023-05-12 12:31:53 +0530

    Update nat.tf

--- nat.tf
+++ nat.tf
@@ -5,7 +5,7 @@ resource "aws_nat_gateway" "nat1" {
   allocation_id = aws_eip.nat-1.id
   subnet_id     = aws_subnet.subnet1.id
   tags = {
-    Name = nat-1
+    Name = "nat-1"
  }  
 }
 
@@ -15,7 +15,7 @@ resource "aws_nat_gateway" "nat2" {
   allocation_id = aws_eip.nat-2.id
   subnet_id     = aws_subnet.subnet2.id
   tags = {
-    Name = nat-2
+    Name = "nat-2"
  }  
 }
 
