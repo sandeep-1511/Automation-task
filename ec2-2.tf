cd27a04292b822f6fe5b03e9dbdcc0dca7493852
sandeep-1511 <108406587+sandeep-1511@users.noreply.github.com>
2023-05-12 13:10:17 +0530

    Update ec2-2.tf

--- ec2-2.tf
+++ ec2-2.tf
@@ -3,7 +3,7 @@ resource "aws_instance" "darwin" {
   ami                         =  "ami-050a217bf9d70aaa6"
   instance_type               = "t2.micro"
   subnet_id                   = aws_subnet.subnet01.id
-  vpc_security_group_ids      = [aws_security_group.ssh2_group.id]
+  vpc_security_group_ids      = [aws_security_group.ssh2.id]
   availability_zone           = "ap-south-1a"
   associate_public_ip_address = true
   key_name                    = "TF-key"
