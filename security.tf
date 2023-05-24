af766639e4ad6733bf695baa618f3094dc87ef19
sandeep-1511 <108406587+sandeep-1511@users.noreply.github.com>
2023-05-24 11:18:56 +0530

    Update security.tf

--- security.tf
+++ security.tf
@@ -10,7 +10,12 @@ ingress {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
-
+ ingress {
+  from_port   = -1
+  to_port     = -1
+  protocol    = "icmp"
+  cidr_blocks = ["aws_security_group.ssh1"]
+  }
   egress {
     from_port   = 0
     to_port     = 0
